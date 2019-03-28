ed976de4236e36f64a493d32eb89236479298a48
Ana Paula de Oliveira <43223750+anniesohma@users.noreply.github.com>
2019-03-28 13:56:00 -0300

    Delete name.js

--- raccoon/name.js
+++
@@ -1,9 +0,0 @@
-var database = data;
-
-database.forEach((prod) => { //substituindo os caracteres 
-	prod.name = prod.name.replace(/æ/g, 'a')
-	prod.name = prod.name.replace(/¢/g, 'c')
-	prod.name = prod.name.replace(/ø/g, 'o')
-	prod.name = prod.name.replace(/ß/g, 'b')
-})
-
